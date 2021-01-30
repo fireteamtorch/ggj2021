9ca0e82f24d1605ddf51e30efc4263d848c775ad
fortunac <cfortuna@bu.edu>
2021-01-30 16:52:19 -0500

    Add potato

---
+++ Assets/Scripts/Potato.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Potato : MonoBehaviour
+{
+    public int x = 42;
+    public SpriteRenderer renderer;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("POTATO");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            renderer.color = Color.red;
+            audioSource.Play();
+        }
+    }
+}
