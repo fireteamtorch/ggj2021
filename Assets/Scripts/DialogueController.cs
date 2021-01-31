939a0f747b8871cb6bf273a86eae5bfc1c0d86cc
Chloe Fortuna <cfortuna@draper.com>
2021-01-30 23:47:32 -0500

    Stub code for dialogue options (#1)

--- Assets/Scripts/DialogueController.cs
+++ Assets/Scripts/DialogueController.cs
@@ -3,7 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using Febucci.UI;
 
-public enum DialogueSlide { NONE, C1_HELLO, C1_HOWSITGOING, C1_GOODBYE, C2_WHATKNIFE}
+public enum DialogueSlide {
+    NONE,
+    IDOL_DESCRIPTION,
+    IDOL_DIALOGUE,
+    PHOTO_DESCRIPTION,
+    PHOTO_DIALOGUE,
+    GAME_DESCRIPTION,
+    GAME_DIALOGUE,
+    SPIRIT_BATTLE_WIN,
+    SPIRIT_BATTLE_LOSE,
+    HELLO,
+    GOODBYE
+};
+public enum DialogueSpeaker { NARRATOR, PROTAGONIST, GHOST };
 
 public class DialogueController : MonoBehaviour
 {
@@ -15,7 +28,7 @@ public class DialogueController : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            if(isTextBoxReadyForNewText && nextSlideID > 0)
+            if (isTextBoxReadyForNewText && nextSlideID > 0)
             {
                 StartNextDialogueBox();
             }
@@ -25,6 +38,7 @@ public class DialogueController : MonoBehaviour
     public void StartNextDialogueBox()
     {
         isTextBoxReadyForNewText = false;
+        Debug.Log(nextSlideID);
         RunSlide((DialogueSlide)nextSlideID);
     }
 
@@ -36,15 +50,56 @@ public class DialogueController : MonoBehaviour
 
     public void RunSlide(DialogueSlide aSlide)
     {
+        Debug.Log(aSlide);
         switch (aSlide)
         {
-            case DialogueSlide.C1_HELLO:
-                dialoguePlayer.ShowText("Hey, I'm a ghost!!");
-                nextSlideID = (int)DialogueSlide.C1_GOODBYE;
+            case DialogueSlide.IDOL_DESCRIPTION:
+                dialoguePlayer.ShowText("Description for idol album");
+                nextSlideID = (int)DialogueSlide.NONE;
                 break;
 
-            case DialogueSlide.C1_GOODBYE:
-                dialoguePlayer.ShowText("Cya");
+            case DialogueSlide.IDOL_DIALOGUE:
+                dialoguePlayer.ShowText("Ghost talks about her idol life");
+                nextSlideID = (int)DialogueSlide.NONE;
+                break;
+
+            case DialogueSlide.PHOTO_DESCRIPTION:
+                dialoguePlayer.ShowText("Description about her family");
+                nextSlideID = (int)DialogueSlide.NONE;
+                break;
+
+            case DialogueSlide.PHOTO_DIALOGUE:
+                dialoguePlayer.ShowText("Ghost talks about her family");
+                nextSlideID = (int)DialogueSlide.NONE;
+                break;
+
+            case DialogueSlide.GAME_DESCRIPTION:
+                dialoguePlayer.ShowText("Description about the video game controller");
+                nextSlideID = (int)DialogueSlide.NONE;
+                break;
+
+            case DialogueSlide.GAME_DIALOGUE:
+                dialoguePlayer.ShowText("Ghost talks about how she wanted to video game with her sister");
+                nextSlideID = (int)DialogueSlide.NONE;
+                break;
+
+            case DialogueSlide.SPIRIT_BATTLE_WIN:
+                dialoguePlayer.ShowText("You win!");
+                nextSlideID = (int)DialogueSlide.NONE;
+                break;
+
+            case DialogueSlide.SPIRIT_BATTLE_LOSE:
+                dialoguePlayer.ShowText("You lose!");
+                nextSlideID = (int)DialogueSlide.NONE;
+                break;
+
+            case DialogueSlide.HELLO:
+                dialoguePlayer.ShowText("Hey!");
+                nextSlideID = (int)DialogueSlide.GOODBYE;
+                break;
+
+            case DialogueSlide.GOODBYE:
+                dialoguePlayer.ShowText("BYE!");
                 nextSlideID = (int)DialogueSlide.NONE;
                 break;
 
