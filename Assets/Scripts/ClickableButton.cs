17a3a9518bdb6e278844c8ee647981a116317808
Jeffrey Hsu <silverhsu@gmail.com>
2021-01-30 22:18:44 -0600

    Room buttons and dialogue animations

---
+++ Assets/Scripts/ClickableButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum RoomButtonType {  NONE, KNIFE, SHEATH, BLOOD, DOORKNOB}
+
+public class ClickableButton : MonoBehaviour
+{
+    public RoomButtonType thisButtonType;
+    private Collider2D thisCol;
+    private RoomController roomCtrl;
+
+    private void Start()
+    {
+        roomCtrl = GameObject.FindObjectOfType<RoomController>();
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            roomCtrl.ReportRoomButtonPressed(thisButtonType);
+        }
+    }
+}
---
+++ Assets/Scripts/RoomController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    public bool isAcceptingRoomButtons;
+    public bool hasKnifeBeenChecked;
+
+
+    void Start()
+    {
+        isAcceptingRoomButtons = true;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ReportRoomButtonPressed(RoomButtonType aButtonType)
+    {
+        if (isAcceptingRoomButtons)
+        {
+            Debug.Log("Button: " + aButtonType.ToString());
+            switch (aButtonType)
+            {
+                case RoomButtonType.KNIFE:
+                    if (!hasKnifeBeenChecked)
+                    {
+                        // DIALOGUE FOR KNIFE DESCRIPT HERE
+                        hasKnifeBeenChecked = true;
+                    }
+                    else
+                    {
+                        // DIALOGUE FOR KNIFE REVEAL
+                    }
+
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
