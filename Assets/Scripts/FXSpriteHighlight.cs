f063c5cfca04aa02eb34f1cac37556b7a92d578b
Jeffrey Hsu <silverhsu@gmail.com>
2021-01-30 18:19:20 -0600

    Potato and spirit start

---
+++ Assets/Scripts/FXSpriteHighlight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FXSpriteHighlight : MonoBehaviour
+{
+    public float offsetRadius;
+    public float offsetZ;
+    private float prevZAngle;
+
+
+    void Update()
+    {
+        if (this.transform.rotation.eulerAngles.z != prevZAngle)
+        {
+            prevZAngle = this.transform.rotation.eulerAngles.z;
+            this.transform.localPosition = new Vector3(0f, 0f, offsetZ) + transform.InverseTransformDirection(Vector3.up).normalized * offsetRadius;
+        }
+    }
+}
---
+++ Assets/Scripts/PotatoDropper.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PotatoDropper : MonoBehaviour
+{
+    public GameObject potatoPrefab;
+    public GameObject spawnAnchor;
+
+    private float spawnRangeLimit = 5.2f;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Instantiate(potatoPrefab, 
+                spawnAnchor.transform.position + new Vector3(Random.Range(-spawnRangeLimit, spawnRangeLimit),0f,0f), 
+                Quaternion.identity);
+        }
+    }
+}
